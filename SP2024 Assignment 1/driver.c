11e54a822d0d96bdede8af8e1e8cbb84ddd090be
K4le21 <116043633+K4le21@users.noreply.github.com>
2024-01-31 20:31:18 -0800

    Add files via upload

---
+++ SP2024 Assignment 1/driver.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+extern double average();
+
+int main() {
+    printf("Welcome to shitass maintained by my asshole\n");
+
+    double randomNumber = average();
+
+    printf("The driver has received this number %lf and will keep it for future use.\n", randomNumber);
+    printf("Have a great day.\nA zero will be sent to the operating system as a signal of a successful execution.\n");
+
+    return 0;
+}
